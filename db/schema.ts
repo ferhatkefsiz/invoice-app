e150818ca89ae1e1439ad6d597a95b93255fa4ac
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-06 16:33:38 +0300

    feat: add customers schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
 import { text } from "drizzle-orm/pg-core";
 import { timestamp } from "drizzle-orm/pg-core";
-import { integer, pgEnum, pgTable } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, serial, integer } from "drizzle-orm/pg-core";
 
 import { AVAILABLE_STATUSES } from "@/consts";
 
@@ -20,4 +20,13 @@ export const Invoices = pgTable("invoices", {
   value: integer("value").notNull(),
   description: text("description").notNull(),
   status: statusEnum("status").notNull(),
+});
+
+export const Customers = pgTable("customers", {
+  id: serial("id").primaryKey().notNull(),
+  createTs: timestamp("createTs").defaultNow().notNull(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  userId: text("userId").notNull(),
+  organizationId: text("organizationId"),
 });
