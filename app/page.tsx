2aa3adc2117937e594e05affdfc1e68fbfc88acf
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-06 17:47:41 +0300

    refactor: Remove unused code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto px-5 min-h-screen flex flex-col justify-center h-full text-center items-center">
       <h1 className="text-5xl font-bold">Invoice App</h1>
-      <Button asChild>
-        <Link href="/dashboard">Go to Dashboard</Link>
-      </Button>
     </main>
   );
 }
