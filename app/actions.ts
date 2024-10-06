25585f3448b7efdf7a412652157b033a818d6c17
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-06 17:01:50 +0300

    refactor: Update 'invoices' table schema and form submission handling

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,16 @@
 "use server";
 
-import { db } from "@/db";
-import { auth } from "@clerk/nextjs/server";
-import { Customers, Invoices } from "@/db/schema";
-import { Resend } from "resend";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@clerk/nextjs/server";
+import { Resend } from "resend";
+
+import { db } from "@/db";
+import { Customers, Invoices, type Status } from "@/db/schema";
+
+import { and, eq, isNull } from "drizzle-orm";
+
 import InvoiceCreatedEmail from "@/emails/invoice-created";
 
 // const stripe = new Stripe(String(process.env.STRIPE_API_SECRET));
@@ -62,4 +68,42 @@ export async function createAction(formData: FormData) {
   });
 
   redirect(`/invoices/${results[0].id}`);
+}
+export async function updateStatusAction(formData: FormData) {
+  const { userId, orgId } = auth();
+
+  // Updating disabled for demo
+  if (userId !== process.env.ME_ID) return;
+
+  if (!userId) {
+    return;
+  }
+
+  const id = formData.get("id") as string;
+  const status = formData.get("status") as Status;
+
+  if (orgId) {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.organizationId, orgId)
+        )
+      );
+  } else {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.userId, userId),
+          isNull(Invoices.organizationId)
+        )
+      );
+  }
+
+  revalidatePath(`/invoices/${id}`, "page");
 }
