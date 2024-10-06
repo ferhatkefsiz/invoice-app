2d751e8b41f50343098182642d2ced7464c0f90e
ferhatkefsiz <ferhatkefsiz@yahoo.com>
2024-10-06 18:14:46 +0300

    refactor: sign in page

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,9 @@ const Footer = () => {
   return (
     <footer className="mt-12 mb-8">
       <div className="max-w-5xl mx-auto px-5 flex justify-between gap-4">
+        <p className="text-sm">Invoices App</p>
         <p className="text-sm">
-          Invoicipedia &copy; {new Date().getFullYear()}
-        </p>
-        <p className="text-sm">
-          Created by Colby Fayock with Next.js, Xata, and Clerk
+          &copy; {new Date().getFullYear()} All rights reserved.
         </p>
       </div>
     </footer>
